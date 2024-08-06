58021fed027000a0e3ce0d884e8fdaba392374d9
Dave Mac <kreeckinc@gmail.com>
2024-08-06 11:57:14 +0200

    removing the green blue deplyment method

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Generate target_env.conf based on TARGET_ENV environment variable
-echo "set \$target_env ${TARGET_ENV};" > /etc/nginx/conf.d/target_env.conf
+# Generate the target_env.conf file based on TARGET_ENV environment variable
+echo "set \$TARGET_ENV ${TARGET_ENV};" > /etc/nginx/conf.d/target_env.conf
 
 # Start Nginx
 nginx -g 'daemon off;'
