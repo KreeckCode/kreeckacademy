218e2e32e3552d604c4a615a5deba2cace9b4c35
Dave Mac <kreeckinc@gmail.com>
2024-08-10 14:33:33 +0200

    reverting to the old docker settings

--- nginx/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Generate the target_env.conf file based on TARGET_ENV environment variable
-echo "set \$TARGET_ENV ${TARGET_ENV};" > /etc/nginx/conf.d/target_env.conf
-
-# Start Nginx
-nginx -g 'daemon off;'
